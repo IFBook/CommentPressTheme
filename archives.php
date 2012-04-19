00ba4e56b30eda6ab83c5d5aac2612dacc85081c
Christian Wach <needle@haystack.co.uk>
2012-04-19 10:12:26 +0100

    added categories and tags to archive page

--- archives.php
+++ archives.php
@@ -53,6 +53,92 @@ get_header(); ?>
 
 
 
+<div class="categories">
+
+<h3>Categories</h3>
+
+<ul>
+<?php 
+
+// configure
+$defaults = array(
+
+'show_option_all' => '', 
+'orderby' => 'name',
+'order' => 'ASC', 
+'show_last_update' => 0,
+'style' => 'list', 
+'show_count' => 0,
+'hide_empty' => 1, 
+'use_desc_for_title' => 1,
+'child_of' => 0, 
+'feed' => '', 
+'feed_type' => '',
+'feed_image' => '', 
+'exclude' => '', 
+'exclude_tree' => '', 
+'current_category' => 0,
+'hierarchical' => true, 
+'title_li' => '',
+'echo' => 1, 
+'depth' => 0
+
+);
+
+// show them
+wp_list_categories( $defaults ); 
+
+?>
+</ul>
+
+</div>
+
+
+
+<div class="tags">
+
+<h3>Tags</h3>
+
+<?php 
+
+//echo get_the_tag_list('<ul><li>','</li><li>','</li></ul>');
+
+$args = array(
+
+	'smallest' => 1.1, 
+	'largest' => 1.1,
+	'unit' => 'em', 
+	'number' => 0,  
+	'format' => 'list',
+	'separator' => '\n',
+	'orderby' => 'name', 
+	'order' => 'ASC',
+	'link' => 'view', 
+	'taxonomy' => 'post_tag', 
+	'echo' => false
+
+);
+
+// get them
+$tags = wp_tag_cloud( $args );
+
+// did we get any?
+if ( $tags != '' ) {
+
+	echo $tags;
+
+} else {
+
+	echo '<ul><li class="no_tags">No tags yet</li></ul>';
+
+}
+
+?>
+
+</div>
+
+
+
 </div><!-- /post -->
 
 </div><!-- /content -->
