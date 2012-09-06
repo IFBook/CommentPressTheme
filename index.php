48ddd40d61cee29753e1acd0eb3707cf7dbef1fe
Christian Wach <needle@haystack.co.uk>
2012-09-06 16:25:09 +0100

    cleaned out legacy code from index template

--- index.php
+++ index.php
@@ -22,91 +22,49 @@
 
 
 
-<?php
+<?php if (have_posts()) : ?>
 
-// init id
-$title_page_id = '';
+	<?php while (have_posts()) : the_post(); ?>
 
-// declare access to globals
-global $commentpress_obj;
+	<div class="search_result">
 
-// if we have the plugin enabled...
-if ( is_object( $commentpress_obj ) ) {
-
-	// get ID of title page
-	//$title_page_id = $commentpress_obj->db->option_get( 'cp_welcome_page' );
-	
-}
-
-
-
-// have we set a title page?
-if ( $title_page_id != '' ) {
-
-	// get title page
-	$title = get_page( $title_page_id );
-	
-	// show content
-	setup_postdata( $title );
-	
-	// enable Wordpress API on page
-	$GLOBALS['post'] = $title;
-
-	// show page content
-	?><h2 class="post_title"><?php the_title(); ?></h2>
-	
-	<?php the_content('Read the rest of this entry &raquo;'); ?>
-
-	<?php edit_post_link('Edit this entry', '<p class="edit_link">', '</p>'); 
-	
-} else {
-
-	// show river of news
-	if (have_posts()) : ?>
-
-		<?php while (have_posts()) : the_post(); ?>
-
-		<div class="search_result">
-
-			<h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-			
-			<div class="search_meta">
-				
-				<?php cp_echo_post_meta(); ?>
-				
-			</div>
-
-			<?php the_excerpt() ?>
+		<h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
 		
-			<p class="search_meta"><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></p>
-		
-		</div><!-- /search_result -->
+		<div class="search_meta">
+			
+			<?php cp_echo_post_meta(); ?>
+			
+		</div>
+
+		<?php the_excerpt() ?>
 	
-		<?php endwhile; ?>
+		<p class="search_meta"><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></p>
+	
+	</div><!-- /search_result -->
 
-	<?php else : ?>
+	<?php endwhile; ?>
 
-			<h2 class="post_title">No blog posts found</h2>
+<?php else : ?>
+
+		<h2 class="post_title">No blog posts found</h2>
+		
+		<p>There are no blog posts yet. <?php
+		
+		// if logged in
+		if ( is_user_logged_in() ) {
 			
-			<p>There are no blog posts yet.<?php
+			// add a suggestion
+			?> <a href="<?php admin_url(); ?>">Go to your dashboard to add one.</a><?php
 			
-			// if logged in
-			if ( is_user_logged_in() ) {
-				
-				// add a suggestion
-				?> <a href="<?php admin_url(); ?>">Go to your dashboard to add one.</a><?php
-				
-			}
-				
-			?></p>
+		}
 			
-			<p>If you were looking for something that hasn't been found, try using the search form below.</p>
+		?></p>
+		
+		<p>If you were looking for something that hasn't been found, try using the search form below.</p>
 
-			<?php get_search_form(); ?>
+		<?php get_search_form(); ?>
 
-	<?php endif; ?>
-
-<?php } ?>
+<?php endif; ?>
 
 
 
